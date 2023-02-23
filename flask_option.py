e301a24103bfb56a7a9ffd82b451f4a73f97e657
gonzalo lara <gonzaloflara@gmail.com>
2023-02-23 18:28:54 -0300

    done

--- flask_option.py
+++ flask_option.py
@@ -4,7 +4,12 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    environment = os.getenv("FLASK_ENV")
+    if environment == "development":
+        return "Starting in development mode..."
+    if environment == "production":
+        return "Starting in production mode..."
+    return "Starting in empty mode..."
 
 if __name__ == "__main__":
     print(start())
